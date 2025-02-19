42a9a3caac81ce806e716fd1b8c4123b9c9a4569
chaeyun <hur6149@naver.com>
2025-02-19 23:17:25 +0900

    Only FE

--- src/routes/imageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadImage } = require('../controllers/imageController');
-
-// POST /api/image
-router.post('/', uploadImage);
-
-module.exports = router;
